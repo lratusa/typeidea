f7d1db25980001f7cd8ab876ff2921c9a98e0986
lratusa <lratusa@126.com>
2019-09-28 03:21:29 +0800

    加了一些html用于前端显示

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -20,7 +20,21 @@ class Category(models.Model):
 		verbose_name = verbose_name_plural = '分类'
 
 	def __str__(self):
-		return self.name	
+		return self.name
+	@classmethod
+	def get_navs(cls):
+		categories = cls.objects.filter(status=cls.STATUS_NORMAL)
+		nav_categories = []
+		normal_categories = []
+		for cate in categories: #这样只有一次query
+			if cate.is_nav:
+				nav_categories.append(cate)
+			else:
+				normal_categories.append(cate)
+		return {
+			'navs': nav_categories,
+			'categories': normal_categories,
+		}
 
 class Tag(models.Model):
 	STATUS_NORMAL = 1
@@ -61,6 +75,8 @@ class Post(models.Model):
 	tag = models.ManyToManyField(Tag, verbose_name="标签")
 	owner = models.ForeignKey(User, verbose_name="作者")
 	created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+	pv = models.PositiveIntegerField(default=1)
+	uv = models.PositiveIntegerField(default=1)
 
 	class Meta:
 		verbose_name = verbose_name_plural = "文章"
@@ -68,4 +84,38 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	@staticmethod
+	def get_by_tag(tag_id):
+		try:
+			tag = Tag.objects.get(id=tag_id)
+		except Tag.DoesNotExist:
+			tag = None
+			post_list = []
+		else:
+			post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)\
+				.prefetch_related('owner','tag')
+
+		return post_list, tag
+
+	@staticmethod
+	def get_by_category(category_id):
+		try:
+			category = Category.objects.get(id=category_id)
+		except Category.DoesNotExist:
+			category = None
+			post_list = []
+		else:
+			post_list = category.post_set.filter(status=Post.STATUS_NORMAL)\
+				.select_related('owner','category')
+
+		return post_list, category
+
+	@classmethod
+	def latest_posts(cls):
+		queryset = cls.objects.filter(status=cls.STATUS_NORMAL)
+		return queryset
+
+	def hot_posts(cls):
+		return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
 # Create your models here.
